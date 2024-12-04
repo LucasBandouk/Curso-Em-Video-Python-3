4a515e775b701071fb3f6da3656f188bc1c5c341
Lucas Bandouk <lucas.bandouk@gmail.com>
2024-12-04 10:07:09 -0300

    Iniciando mais um tema sobra python

---
+++ ExerciciosAnotacoes/curso_python/aula22/aula22.py
@@ -0,0 +1,18 @@
+'''Nessa aula, vamos continuar nossos estudos de funções em Python, aprendendo como criar módulos em Python e reutilizar nossos códigos em outros projetos. Vamos aprender também como agrupar vários módulos em um pacote, ampliando ainda mais a modularização em grandes projetos em Python.'''
+
+#Surgio no início da década de 60
+
+#Sistemas ficando cada vez maiores
+
+#Foco: dividir um programa grande
+
+#Foco: aumentar a legibilidade
+
+#Foco: facilitar a manutenção
+
+#import uteis
+
+
+num = int(input("Digite um valor: "))   
+#fat = uteis.fatorial(num)
+#print(f'O fatorial de {num} é {fat}')
---
+++ ExerciciosAnotacoes/curso_python/aula22/modulos/moeda.py
@@ -0,0 +1,11 @@
+def aumenta(preco,taxa):
+    return preco+(preco*taxa/100)
+
+def diminuir(preco,taxa):
+    return preco-(preco*taxa/100)
+
+def dobro(preco):
+    return preco*2
+
+def metade(preco):
+    return preco/2
